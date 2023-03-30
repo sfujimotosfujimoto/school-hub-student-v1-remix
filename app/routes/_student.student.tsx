61060debaa27b0bc1c1ffb6994ef8439fa6ad845
sfujimotosfujimoto <unicoshun@gmail.com>
2023-03-30 18:39:59 +0900

    update bg color

--- app/routes/_student.student.tsx
+++ app/routes/_student.student.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "@remix-run/react"
 
 export default function StudentLayout() {
   return (
-    <div className='m-12 mx-auto flex h-full w-10/12 flex-col items-center justify-center bg-base-100 sm:w-10/12 md:w-3/4'>
+    <div className='m-12 mx-auto flex h-full w-10/12 flex-col items-center justify-center  sm:w-10/12 md:w-3/4'>
       <Outlet />
     </div>
   )
