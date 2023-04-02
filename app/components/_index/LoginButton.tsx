7310d7d55b643a9e5c0ae9297101b643dcc0cd62
sfujimotosfujimoto <unicoshun@gmail.com>
2023-04-02 12:28:02 +0900

    update login btn

--- app/components/_index/LoginButton.tsx
+++ app/components/_index/LoginButton.tsx
@@ -1,4 +1,4 @@
-import { useMatches, useSearchParams } from "@remix-run/react"
+import { Form, useMatches, useSearchParams } from "@remix-run/react"
 
 import Logo from "../icons/Logo"
 import Toast from "../util/Toast"
@@ -14,10 +14,26 @@ export default function LoginButton() {
   return (
     <>
       <div className='relative flex w-full items-center justify-center gap-8 '>
-        <a href='/auth/signin' className={`btn-success btn w-64 shadow-lg`}>
-          <Logo className='h-7 w-4' />
-          <span className=' ml-2 sm:ml-4 sm:inline'>Sign in to SCHOOL HUB</span>
-        </a>
+        {!user ? (
+          <a href='/auth/signin' className={`btn-success btn w-64 shadow-lg`}>
+            <Logo className='h-7 w-4' />
+            <span className=' ml-2 sm:ml-4 sm:inline'>
+              Sign in to SCHOOL HUB
+            </span>
+          </a>
+        ) : (
+          <Form method='post' action='/auth/signout'>
+            <button
+              type='submit'
+              className={`btn-error btn w-64  border-0 shadow-lg hover:bg-opacity-70`}
+            >
+              <Logo className='h-7 w-4' />
+              <span className=' ml-1 sm:ml-2 sm:inline'>
+                Sign out of SCHOOL HUB
+              </span>
+            </button>
+          </Form>
+        )}
         {login === "false" && <Toast text='You have to login!' />}
         {expired && <Toast text='Your token has expired!' />}
         {!user && <Toast text='You have to login!' />}
