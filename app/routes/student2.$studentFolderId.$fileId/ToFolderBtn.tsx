cfc7aa4124b4a4e382bca5b64ec3e3ed3240fa5c
sfujimotosfujimoto <unicoshun@gmail.com>
2023-12-25 15:55:55 +0900

    clean up tofolderbtn

--- app/routes/student2.$studentFolderId.$fileId/ToFolderBtn.tsx
+++ app/routes/student2.$studentFolderId.$fileId/ToFolderBtn.tsx
@@ -1,17 +1,16 @@
+import { AButton } from "~/components/buttons/button"
 import FolderIcon from "~/components/icons/folder-icon"
 
 export default function ToFolderBtn({ parentId }: { parentId: string }) {
   return (
-    <a
-      target="_blank"
-      rel="noopener noreferrer"
+    <AButton
       href={`https://drive.google.com/drive/folders/${parentId}`}
-      className={`  h-full rounded-lg bg-sfgreen-200 px-2 py-3 shadow-md transition-all duration-500  hover:-translate-y-1 hover:bg-sfgreen-400`}
+      size="sm"
     >
       <div className="flex items-center justify-center">
         <FolderIcon className="mr-2 h-6 w-6" />
         フォルダへ
       </div>
-    </a>
+    </AButton>
   )
 }
