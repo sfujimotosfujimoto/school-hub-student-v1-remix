d83a541820f8c96a943d86598a2e5aa4d3672f3d
sfujimotosfujimoto <unicoshun@gmail.com>
2023-12-27 12:43:50 +0900

    error daisy

--- app/routes/auth.signout/route.tsx
+++ app/routes/auth.signout/route.tsx
@@ -1,3 +1,9 @@
+
+🌼   daisyUI 4.4.24
+├─ ✔︎ 1 theme added		https://daisyui.com/docs/themes
+╰─ ★ Star daisyUI on GitHub	https://github.com/saadeghi/daisyui
+
+// todo
 import type { ActionFunctionArgs } from "@remix-run/node"
 import { signout } from "~/lib/signinout.server"
 
