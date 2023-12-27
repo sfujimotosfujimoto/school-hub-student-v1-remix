57e31141a42c0047de36028fec4dc506b9ad609d
sfujimotosfujimoto <unicoshun@gmail.com>
2023-12-27 18:03:43 +0900

    delete prac file

---
+++ app/routes/auth.signout/route2.tsx
@@ -0,0 +1,7 @@
+import type { ActionFunctionArgs } from "@remix-run/node"
+
+import { signout } from "~/lib/signinout.server"
+
+export function action({ request }: ActionFunctionArgs) {
+  return signout({ request })
+}
