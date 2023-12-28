f675feab992430a7bf4736fc1b6c9b0e3fa91cf6
sfujimotosfujimoto <unicoshun@gmail.com>
2023-12-28 21:21:56 +0900

    change return type of signin()

--- app/routes/auth.redirect/route.tsx
+++ app/routes/auth.redirect/route.tsx
@@ -4,6 +4,9 @@ import { signin } from "~/lib/signinout.server"
 
 import ErrorBoundaryDocument from "~/components/error-boundary-document"
 import { redirectToSignin } from "~/lib/responses"
+import { createUserSession } from "~/lib/services/session.server"
+import { getDriveFiles } from "~/lib/google/drive.server"
+import { saveDriveFileData } from "~/lib/services/drive-file-data.server"
 
 export async function loader({ request }: LoaderFunctionArgs) {
   // get code from url query
@@ -13,7 +16,23 @@ export async function loader({ request }: LoaderFunctionArgs) {
   // if no "code" , do not touch and resolve
   if (!code) throw redirectToSignin()
 
-  return signin({ code })
+  const { folderId, userJWT, accessToken, userId } = await signin({ code })
+
+  if (folderId === null && userJWT.trim()) {
+    return createUserSession(userJWT, `/admin`)
+  }
+
+  // Get drive files from Google Drive API
+  const driveFiles = await getDriveFiles(
+    accessToken,
+    `trashed=false and '${folderId}' in parents`,
+  )
+
+  // Save drive files to DB
+  await saveDriveFileData(userId, driveFiles)
+  // const driveFileData = await saveDriveFileData(userId, driveFiles)
+
+  return createUserSession(userJWT, `/student/${folderId}`)
 }
 
 export default function Redirect() {
