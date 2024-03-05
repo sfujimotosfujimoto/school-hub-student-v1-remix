24b5f312d198b0e8a27b561f62bb659a8713e2f6
sfujimotosfujimoto <unicoshun@gmail.com>
2024-03-05 14:42:07 +0900

    refac

--- app/routes/student.$studentFolderId/route.tsx
+++ app/routes/student.$studentFolderId/route.tsx
@@ -6,10 +6,6 @@ import { CACHE_MAX_AGE_SECONDS } from "~/config"
  * Meta Function
  */
 export const meta: MetaFunction = () => {
-  // const title =
-  //   `${data?.student.gakunen}${data?.student.hr}${data?.student.hrNo}${data?.student.last}${data?.student.first}` ||
-  //   ""
-
   return [
     {
       title: `SCHOOL HUB`,
@@ -46,41 +42,3 @@ export function ErrorBoundary() {
   let message = `フォルダID（${studentFolderId}）からフォルダを取得できませんでした。`
   return <ErrorBoundaryDocument message={message} />
 }
-
-// /**
-//  * LOADER function
-//  */
-// export async function loader({ request, params }: LoaderFunctionArgs) {
-//   logger.debug(
-//     `🍿 loader: student.$studentFolderId ${new URL(request.url).href}`,
-//   )
-//   const { user } = await getUserFromSession(request)
-//   if (!user || !user.credential) throw redirectToSignin(request)
-//   await requireUserRole(request, user)
-
-//   const student = user.student
-
-//   if (!student || !student.folderLink) throw redirectToSignin(request)
-
-//   // Check if studentFolderId is the same as the student's folder
-//   if (getFolderId(student.folderLink) !== params.studentFolderId) {
-//     throw redirectToSignin(request)
-//   }
-
-//   const studentFolderId = params.studentFolderId
-//   invariant(studentFolderId, "studentFolder in params is required")
-
-//   const headers = new Headers()
-
-//   headers.set("Cache-Control", `private, max-age=${CACHE_MAX_AGE_SECONDS}`)
-//   return json(
-//     {
-//       studentFolderId: params.studentFolderId,
-//       student,
-//       role: user.role,
-//     },
-//     {
-//       headers,
-//     },
-//   )
-// }
